15389f488b03021ea50eebc5ab5e67fd2e5dfdb9
spai <tobbe@spai.se>
2020-11-16 00:19:14 +0100

    Layout demo page

---
+++ samples/SharedDemo/Demos/Layout.razor.cs
@@ -0,0 +1,35 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Layout;
+using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Core.Models.Core;
+using Microsoft.AspNetCore.Components;
+
+namespace SharedDemo
+{
+    public class LayoutComponent : ComponentBase
+    {
+        protected readonly DiagramManager diagramManager = new DiagramManager();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            var node1 = NewNode(50, 50);
+            var node2 = NewNode(300, 300);
+            diagramManager.AddLink(node1.GetPort(PortAlignment.Right), node2.GetPort(PortAlignment.Left));
+            diagramManager.AddNode(node1);
+            diagramManager.AddNode(node2);
+            diagramManager.AddNode(NewNode(300, 50));
+        }
+
+        private NodeModel NewNode(double x, double y)
+        {
+            var node = new NodeModel(new Point(x, y));
+            node.AddPort(PortAlignment.Bottom);
+            node.AddPort(PortAlignment.Top);
+            node.AddPort(PortAlignment.Left);
+            node.AddPort(PortAlignment.Right);
+            return node;
+        }
+    }
+}
